cc85e65a46d5cc04dbb5d321abc0e6389db106dd
SirrScott <147071346+SirrScott@users.noreply.github.com>
2024-10-30 16:31:31 +0000

    removed combined age, split up rule set for ring-physical

--- forward-chaining/AI-ForwardChaining.py
+++ forward-chaining/AI-ForwardChaining.py
@@ -1,56 +1,39 @@
 
 class agePrediction:
     def __init__(self,length,weight,rings):
-        self.length = length
-        self.weight = weight
+        self.length = length  #mm
+        self.weight = weight  #g
         self.rings = rings
         self.ringAge = None
         self.physicalAge = None
-        self.combinedAge = None
 
-    #less than 6 rings (<7.5 years old),  young
-    #from 6 to 13 rings (7.5 to 14.5 years old),  adult
-    #and more than 13 rings (>14.5 years old),  old
-
-    def rules(self):
-        if self.rings >= 13:
+    def ringRules(self):
+        if self.rings >= 16:
             print("1")
             self.ringAge = "old"
-            self.physRules()
-        elif self.rings >= 6:
+        elif self.rings >= 9:
             print("2")
             self.ringAge = "adult"
-            self.physRules()
-        elif self.rings < 6:
+        elif self.rings < 9:
             print("3")
             self.ringAge = "young"
-            self.physRules()
         else:
-            print("err")
-            self.combinedAge = "unknown"
-
-        if self.ringAge != self.physicalAge:
-            self.combinedAge = "unknown"
+            print("err1")
+            self.ringAge = "unknown"
 
     def physRules(self):
-        if self.length >= 0.6 and self.weight >= 0.5:
+        if self.length >= 0.6 and self.weight >= 1.5:
             print("1.1")
             self.physicalAge = "old"
-            self.combinedAge = "old"
-        elif self.length >= 0.4 and self.weight >= 0.3:
+        elif self.length >= 0.35 and self.weight >= 0.5:
             print("2.1")
             self.physicalAge = "adult"
-            self.combinedAge = "adult"
-        elif self.length < 0.4 and self.weight < 0.3:
+        elif self.length < 0.35 and self.weight < 0.5:
             print("3.1")
             self.physicalAge = "young"
-            self.combinedAge = "young"
         else:
-            print("err")
-
-    def predict(self):
-        self.rules()
-        return self.combinedAge
+            print("err2")
+            self.physicalAge = "unknown"
 
     def getRing(self):
         return self.ringAge
@@ -60,11 +43,14 @@ class agePrediction:
 
 
 abalone1 = agePrediction(length = 0.5, weight = 0.3, rings = 13)
-prediction = abalone1.predict()
-if prediction == "unknown":
-    print("physical and ring age classifications do not match")
-    print(f"ring predictions have the age at: {abalone1.getRing()}")
+abalone1.ringRules()
+abalone1.physRules()
+
+if abalone1.getRing() != abalone1.getPhysical():
+    print("predictions with physical attributes dont match true age given by ring age")
+    print(f"ring method has the age at: {abalone1.getRing()}")
     print(f"while physical attributes show it to be: {abalone1.getPhysical()}")
-    print("true age is uncertain")
-else:
-    print(f"age of abalone is: {prediction}")
+elif abalone1.getRing() == abalone1.getPhysical():
+    print("physical attribute age prediction match actual age from rings")
+    print(f"ring method has the age at: {abalone1.getRing()}")
+    print(f"while physical attributes show it to be: {abalone1.getPhysical()}")
